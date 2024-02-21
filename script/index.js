47ad8fdd3fbc2c3f603094367d15cbed0a831e3d
mumin6 <am.mumin99@outlook.com>
2024-02-21 19:45:57 +0600

    finished

--- script/index.js
+++ script/index.js
@@ -1,39 +1,47 @@
 ;
 const ticketPrice = 550;
 const ticketClass = "Economy";
+
 function selectSeat(id) {
-    const btnName = document.getElementById(id).innerHTML;
+    const seat3 = document.getElementById("seat-selected").innerText;
+    const seat4 = parseInt(seat3);
 
-    const show = document.getElementById("selected");
+    if (seat4 < 4) {
+        const btnName = document.getElementById(id).innerHTML;
 
-    const div = document.createElement("div");
-    show.appendChild(div);
-    div.classList.add("flex");
-    div.classList.add("justify-between");
-    const p = document.createElement("p");
-    p.innerText = btnName;
-    const p1 = document.createElement("p");
-    p1.innerText = ticketClass;
-    const p2 = document.createElement("p");
-    p2.innerText = ticketPrice;
-    div.appendChild(p);
-    div.appendChild(p1);
-    div.appendChild(p2);
-    totalPrice();
-    grandTotal();
+        const show = document.getElementById("selected");
 
-    const seat = document.getElementById("seat-left").innerText;
-    document.getElementById("seat-left").innerText = parseInt(seat) - 1;
+        const div = document.createElement("div");
+        show.appendChild(div);
+        div.classList.add("flex");
+        div.classList.add("justify-between");
+        const p = document.createElement("p");
+        p.innerText = btnName;
+        const p1 = document.createElement("p");
+        p1.innerText = ticketClass;
+        const p2 = document.createElement("p");
+        p2.innerText = ticketPrice;
+        div.appendChild(p);
+        div.appendChild(p1);
+        div.appendChild(p2);
+        totalPrice();
+        grandTotal();
 
-    const seat1 = document.getElementById("seat-selected").innerText;
-    document.getElementById("seat-selected").innerText = parseInt(seat1) + 1;
+        const seat = document.getElementById("seat-left").innerText;
+        document.getElementById("seat-left").innerText = parseInt(seat) - 1;
 
-   
-    document.getElementById(id).disabled = true;
-    const disable = document.getElementById(id);
-    disable.classList.add("btn-success");
-    
-    
+        const seat1 = document.getElementById("seat-selected").innerText;
+        const updatedSeat1 = parseInt(seat1) + 1;
+        document.getElementById("seat-selected").innerText = updatedSeat1;
+
+
+        document.getElementById(id).disabled = true;
+        const disable = document.getElementById(id);
+        disable.classList.add("btn-success");
+    } else {
+        alert("you can only buy 4 ticket at a time")
+    }
+
 }
 
 function totalPrice() {
@@ -70,5 +78,23 @@ function grandTotal(someThing) {
 
 }
 
+function showPopup() {
+    const phnNumber = document.getElementById("phone-number").value;
+    const length = phnNumber.length;
+    if (length != 0) {
+        const successSection = document.getElementById('success');
+        successSection.classList.remove('hidden');
+        const mainSection = document.getElementById('main-section');
+        mainSection.classList.add('hidden');
+    } else {
+        alert("Enter phone number")
+    }
 
+}
+function showMainSection() {
+    const successSection = document.getElementById('success');
+    successSection.classList.add('hidden');
+    const mainSection = document.getElementById('main-section');
+    mainSection.classList.remove('hidden');
+}
 
